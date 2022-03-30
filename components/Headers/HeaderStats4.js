65bd489bef820f57a342076ab2390875f2034211
Brendan Sheen <brendan.sheen@gmail.com>
2022-03-30 06:00:50 -0700

    a

--- components/Headers/HeaderStats4.js
+++ components/Headers/HeaderStats4.js
@@ -3,12 +3,12 @@ import React from "react";
 // components import quote? https://zenquotes.io/api/random
 
 import CardStats from "components/Cards/CardStats.js";
-import TutorialNavBar from "components/Navbars/TutorialNavBar.js";
+
 
 export default function HeaderStats() {
   return (
     <>
-    <TutorialNavBar/>
+   
       {/* Header */}
       <div className="relative bg-blueGray-800 md:pt-32 pb-32 pt-12">
         <div className="px-4 md:px-10 mx-auto w-full">
